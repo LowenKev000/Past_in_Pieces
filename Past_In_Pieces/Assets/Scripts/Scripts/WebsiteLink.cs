24d1c85e0a8d08c333a804e695d6b1f182eb29df
Siffs <mccowin1@msu.edu>
2026-04-30 23:50:53 -0400

    Fixed UI + Music and Added Website Link

---
+++ Past_In_Pieces/Assets/Scripts/Scripts/WebsiteLink.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class WebsiteLink : MonoBehaviour
+{
+    public void Openurl()
+    {
+        Application.OpenURL("https://sites.google.com/msu.edu/pastinpieces/home?authuser=2");
+    }
+}
